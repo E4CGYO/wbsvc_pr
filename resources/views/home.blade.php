9548fff92fefc6ad713731d1810cd53ec6e8a86d
miftahuljanahgurl <mj8086722@gmail.com>
2026-01-05 00:52:34 +0800

    frondend_1
    Baru nambah sedikit frondend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,34 @@
 @section('title', 'About')
 
 @section('content')
+    <div class="container my-4">
+        <div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="4000">
+                    <img src="/images/masak1.jpg" class="d-block w-100" alt="Masak 1">
+                </div>
+                <div class="carousel-item" data-bs-interval="4000">
+                    <img src="/images/masak4.jpg" class="d-block w-100" alt="Masak 2">
+                </div>
+                <div class="carousel-item" data-bs-interval="4000">
+                    <img src="/images/gambar3.jpeg" class="d-block w-100" alt="Gambar 3">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
     <h1>ini adalah halaman home</h1>
-
-
-    
 @endsection
