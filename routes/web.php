2fed948f7a8bd1c11f15d6366f0b4874f4d67541
miftahuljanahgurl <mj8086722@gmail.com>
2026-01-10 01:29:40 +0800

    Frondend tambahan

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,42 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 
-<<<<<<< Updated upstream
-=======
-
 /*
 |--------------------------------------------------------------------------
 | HOME
 |--------------------------------------------------------------------------
 */
->>>>>>> Stashed changes
 Route::get('/', [BlogController::class, 'home']);
-Route::get('/post/create', [BlogController::class, 'create']);
-Route::get('/post/{id}', [BlogController::class, 'show']);
 Route::get('/about', [BlogController::class, 'about']);
-Route::post('/post', [BlogController::class, 'store']);
+
+/*
+|--------------------------------------------------------------------------
+| CREATE
+|--------------------------------------------------------------------------
+*/
+Route::get('/post/create', [BlogController::class, 'create']);
+Route::post('/post', [BlogController::class, 'store'])->name('post.store');
+
+/*
+|--------------------------------------------------------------------------
+| READ
+|--------------------------------------------------------------------------
+*/
+Route::get('/menu', [BlogController::class, 'menu']);
+Route::get('/post/{id}', [BlogController::class, 'show'])->name('post.show');
+
+/*
+|--------------------------------------------------------------------------
+| UPDATE
+|--------------------------------------------------------------------------
+*/
 Route::get('/post/{id}/edit', [BlogController::class, 'edit']);
 Route::put('/post/{id}', [BlogController::class, 'update']);
-Route::delete('/post/{id}', [BlogController::class, 'destroy']);
-Route::get('/menu', [BlogController::class, 'menu']);
 
-
+/*
+|--------------------------------------------------------------------------
+| DELETE (FITUR BARU, AMAN)
+|--------------------------------------------------------------------------
+*/
+Route::delete('/post/{id}', [BlogController::class, 'destroy'])
+    ->name('post.destroy');
